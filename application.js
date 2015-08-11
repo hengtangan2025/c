3d078522b1780adbef4ace83f9a7fe4d9737ec7d
libochao <522427432@qq.com>
2015-08-11 16:56:24 +0800

    1

--- application.js
+++ application.js
@@ -1,11 +1,9 @@
-jQuery(document).ready(function{
-  function cnvs_getCoordinates(e){
-    x = e.clientX;
-    y = e.clientY;
-    document.getElementById("xycoordinates").innerHTML = "Coordinates: (" + x + "," + y + ")";
-  }
+function cnvs_getCoordinates(e){
+  x = e.clientX;
+  y = e.clientY;
+  document.getElementById("xycoordinates").innerHTML = "Coordinates: (" + x + "," + y + ")";
+}
 
-  function cnvs_clearCoordinates(){
-    document.getElementById("xycoordinates").innerHTML = "";
-  }
-})
+function cnvs_clearCoordinates(){
+  document.getElementById("xycoordinates").innerHTML = "";
+}
